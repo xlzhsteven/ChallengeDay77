d29868f3a4cca7382bfeaf32d211e7a37d4eaf64
Xiaolong Zhang <xlzhsteven@gmail.com>
2020-09-27 11:38:58 -0700

    Initial Commit

---
+++ ChallengeDay77/ChallengeDay77App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay77App.swift
+//  ChallengeDay77
+//
+//  Created by Xiaolong Zhang on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeDay77/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChallengeDay77
+//
+//  Created by Xiaolong Zhang on 9/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
